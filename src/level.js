7679e0c9391d5618934795cb4ee5202c8eecb665
Thomas <ming100628@gmail.com>
2022-11-27 09:25:56 +0800

    able to count score

--- src/level.js
+++ src/level.js
@@ -1,18 +1,22 @@
 export default class Level {
   constructor(dimensions) {
+    this.score = 0;
     this.dimensions = dimensions;
     this.pipes = [
       {
         x: 480,
         y: Math.random() * 490,
+        c: false,
       },
       {
         x: 480 + 220,
         y: Math.random() * 490,
+        c: false,
       },
       {
         x: 480 + 220 + 220,
         y: Math.random() * 490,
+        c: false,
       },
     ];
   }
@@ -29,11 +33,16 @@ export default class Level {
     for (let i = 0; i < pipes.length; i++) {
       const pipe = pipes[i];
       if (pipe.x < -30) {
-        this.pipes.push({ x: 660, y: Math.random() * 490 });
+        this.pipes.push({ x: 660, y: Math.random() * 490, c: false });
       } else {
+        if (pipe.x - 1 < 160 && pipe.c == false) {
+          this.score += 1;
+          pipe.c = true;
+        }
         this.pipes.push({
           x: pipe.x - 1,
           y: pipe.y,
+          c: pipe.c,
         });
       }
     }
@@ -52,5 +61,9 @@ export default class Level {
   drawBackground(ctx) {
     ctx.fillStyle = "skyblue";
     ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);
+    ctx.fillStyle = "black";
+
+    ctx.font = "48px serif";
+    ctx.fillText(this.score, 10, 50);
   }
 }
